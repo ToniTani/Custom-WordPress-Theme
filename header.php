48e36481ca23bd82ddef53936379b71ed72549ab
ToniTani <toni.tani.tt@gmail.com>
2022-05-11 01:44:05 +0300

    added functions and singletons and classes.

--- header.php
+++ header.php
@@ -14,10 +14,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class('hello-class'); ?>></body>
+<body <?php body_class(); ?>></body>
+
 <?php
 if ( function_exists('wp_body_open') ) {
     wp_body_open();
 }?>
+<div id="page" class="site">
+    <header id="masthead" class="site-header" role="banner">
+        <?php get_template_part('template-parts/header/nav'); ?>
+    </header>
+    <div id="content" class="site-content">
+    </div>
+</div>
 
-<header>Header</header>
---
+++ inc/classes/class-tani-theme.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bootstraps the Theme.
+ *
+ * @package Tani
+ */
+namespace TANI_THEME\Inc;
+
+use TANI_THEME\Inc\Traits\Singleton;
+
+class TANI_THEME {
+    use Singleton;
+
+    public function __construct() // load class.
+    {
+    }
+    protected function set_hooks() { //actions and filters
+
+    }
+}
