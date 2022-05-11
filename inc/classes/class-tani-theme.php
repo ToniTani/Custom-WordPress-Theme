3a9d16c6a8e743bacfc07c7184b98c729497435a
ToniTani <toni.tani.tt@gmail.com>
2022-05-11 03:45:03 +0300

    hooks theme support and custom logo

--- inc/classes/class-tani-theme.php
+++ inc/classes/class-tani-theme.php
@@ -19,6 +19,22 @@ class TANI_THEME {
     }
 
     protected function setup_hooks() {
+
+        add_action('after_setup_theme', [$this, 'setup_theme']);
         //actions and filters
+    }
+    public function setup_theme() {
+        add_theme_support('title-tag');
+
+        add_theme_support('custom-logo', [
+
+            'header-text'          => [ 'site-title', 'site-description'],
+            'height'               => 50,
+            'width'                => 200,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'unlink-homepage-logo' => true,
+
+        ]);
     }
 }
