a5a260d1c52323b62fcbe5cb1f48e757a49c3275
ToniTani <toni.tani.tt@gmail.com>
2022-05-17 23:35:44 +0300

    wrapped bootstrap classes inside php

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -22,19 +22,33 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id);
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <?php
+    if ( ! empty ($header_menus) && is_array($header_menus) ) {
+        ?>
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                </div>
-            </li>
+            <?php
+                foreach ( $header_menus as $menu_item) {
+                    if ( ! $menu_item->menu_item_parent) {
+                        ?>
+                    <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                            </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                        </div>
+                    </li>
+                    <?php
+                    }
+                }
+            ?>
         </ul>
+        <?php
+    }
+    ?>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
