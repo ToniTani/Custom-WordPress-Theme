24dce8e1ff3bbcddc6bc6739d84d5831c4319d13
ToniTani <toni.tani.tt@gmail.com>
2022-05-18 02:44:38 +0300

    wrapped nav links inside nav header

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -27,26 +27,31 @@ class Menus {
                     'tani-footer-menu' => esc_html__( 'Footer Menu', 'tani' ),
                 ]);
     }
-    public function get_menu_id($location) {
-        // get all locations
+    public function get_menu_id( $location ) {
+
+        // Get all locations
         $locations = get_nav_menu_locations();
 
-        // Get object id by locations
-        $menu_id = $locations[$location];
+        // Get object id by location.
+        $menu_id = ! empty($locations[$location]) ? $locations[$location] : '';
 
         return ! empty( $menu_id ) ? $menu_id : '';
+
     }
 
-    public function get_child_menu_items ( $menu_array, $parent_id) {
+    public function get_child_menu_items( $menu_array, $parent_id ) {
+
         $child_menus = [];
 
-        if (! empty ($menu_array) && is_array( $menu_array))  {
-            foreach ($menu_array as $menu){
-                if ( intval( $menu->menu_item_parent) === $parent_id ) {
-                    array_push($shild_menus, $menu) ;
+        if ( ! empty( $menu_array ) && is_array( $menu_array ) ) {
+
+            foreach ( $menu_array as $menu ) {
+                if ( intval( $menu->menu_item_parent ) === $parent_id ) {
+                    array_push( $child_menus, $menu );
                 }
             }
         }
+
         return $child_menus;
     }
 
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -8,16 +8,16 @@
 
 $menu_class = \TANI_THEME\Inc\Menus::get_instance();
 $header_menu_id = $menu_class->get_menu_id('tani-header-menu');
-
 $header_menus = wp_get_nav_menu_items( $header_menu_id);
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
     <?php
     if ( function_exists( 'the_custom_logo' ) ) {
         the_custom_logo();
     }
     ?>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -33,18 +33,35 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id);
                         $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID);
                         $has_children = ! empty( $child_menu_items) && is_array( $child_menu_items);
 
+                        if ( ! $has_children ) {
+                            ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo esc_url($menu_item->url); ?>">
+                                   <?php echo esc_html( $menu_item->title); ?>
+                                </a>
+                            </li>
+                            <?php
+                        } else {
+                            ?>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="<?php echo esc_url($menu_item->url); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <?php echo esc_html( $menu_item->title); ?>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <?php
+                                        foreach ( $child_menu_items as $child_menu_item) {
+                                            ?>
+                                            <a class="dropdown-item" href="<?php echo esc_url($child_menu_items->url);?>">
+                                                <?php echo esc_html( $child_menu_item->title); ?>
+                                            </a>
+                                            <?php
+                                        }
+                                    ?>
+                                </div>
+                            </li>
+                            <?php
+                        }
                         ?>
-                    <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown
-                            </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                        </div>
-                    </li>
                     <?php
                     }
                 }
